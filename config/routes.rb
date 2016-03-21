9728dbb0471822bb0d8dcf95c64c91d3883de9a9
Moses Kim <moseskim92@yahoo.com>
2016-03-21 18:44:57 +0100

    Longest word game using rails finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  get 'game', to: 'longest_word#game'
+
+  get 'score', to: 'longest_word#score'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
